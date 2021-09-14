78006674ccb505a789bd8d7bb90f8fe32bc14f22
saafda <85120441+saafda@users.noreply.github.com>
2021-09-14 16:47:10 +0200

    Delete ProjectEuler_9.py

--- ProjectEuler_9.py
+++
@@ -1,7 +0,0 @@
-
-for a in range(1, 1000):
-    for b in range(1, 1000):
-        for c in range(1, 1000):
-            if a**2 + b**2 == c**2 and a+b+c == 1000:
-                print(a, b, c)
-#result is a = 200, b = 375, c = 425
